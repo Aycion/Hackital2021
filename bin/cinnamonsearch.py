ae5ea7adbe7fdb150e128926305a1fe56cbffb61
Aycion <emery390@gmail.com>
2017-11-11 18:18:47 -0500

    Wrote accessor for API in 'bin/cinnamonsearch.py'

--- bin/cinnamonsearch.py
+++ bin/cinnamonsearch.py
@@ -1,1 +1,19 @@
+import sys
+sys.path.insert(0, '../')
 from ThesaurusAPI.thesaurus import Word
+
+class Query:
+
+    def __init__(self, query, pos = -1):
+        print (query)
+        if len(query.split()) != 1:
+            raise ValueError("Query should only one word.")
+        self.query = query
+        singleWord = self.query
+        self.search = Word(singleWord)
+        print (singleWord)
+        if pos == -1:
+
+            self.syns = self.search.synonyms()
+        else:
+            self.syns = self.search.synonyms('all', partOfSpeech=pos)
